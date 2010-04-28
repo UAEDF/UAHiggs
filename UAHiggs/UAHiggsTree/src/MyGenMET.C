39b654dfe6dfe2ebda54a2367c51c877c471d9c2
selvaggi <>
2010-04-28 09:19:11 +0000

    removed My*.C files

--- UAHiggs/UAHiggsTree/src/MyGenMET.C
+++
@@ -1,9 +0,0 @@
-#include "UAHiggs/UAHiggsTree/interface/MyGenMET.h"
-
-ClassImp(MyGenMET)
-
-MyGenMET::MyGenMET()
-{ }
-
-MyGenMET::~MyGenMET()
-{ }
--- UAHiggs/UAHiggsTree/src/MyMET.C
+++
@@ -1,9 +0,0 @@
-#include "UAHiggs/UAHiggsTree/interface/MyMET.h"
-
-ClassImp(MyMET)
-
-MyMET::MyMET()
-{ }
-
-MyMET::~MyMET()
-{ }
